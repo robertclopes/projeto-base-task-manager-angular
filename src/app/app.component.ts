b6cda5b60ee3473eb6381555cd507c8252c3c1b8
Robert Lopes <robertclopes@gmail.com>
2019-03-02 09:51:13 -0300

    adding model task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,29 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app works!';
+
+  task: Task = {
+    id: 123,
+    title: "tarefas"
+
+
+  };
+
+  task2: Task = new Task(21,'tarefa constructor')
+
+
 }
+
+export class Task{
+  public id: number;
+  public title: string;
+
+  constructor(id: number, title: string){
+    this.id = id;
+    this.title = title;
+  }
+
+} 
