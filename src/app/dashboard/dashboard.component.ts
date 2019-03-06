fd3605d6a6f05259cbaa02f930f88325c5bb43f1
Robert Lopes <robertclopes@gmail.com>
2019-03-06 20:24:03 -0300

    showing important tasks on the dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+
+import { Task } from "../tasks/shared/tasks.model";
+import { TaskService  } from "../tasks/shared/tasks.service";
 
 @Component({
   selector: 'dashboard',
   templateUrl: './dashboard.component.html'
 })
 
-export class DashboardComponent{
-  
+export class DashboardComponent implements OnInit{
+  public tasks: Task[];
+  public constructor(private taskService: TaskService){
+  }
+
+  public ngOnInit(){
+    this.taskService.getImportantTasks()
+      .then((tasks) => this.tasks = tasks);
+  }
 }
+
--- src/app/tasks/shared/tasks.service.ts
+++ src/app/tasks/shared/tasks.service.ts
@@ -31,4 +31,7 @@ export class TaskService{
     return promise;  
   }
       
+  public getImportantTasks(): Promise<Task[]>{
+    return Promise.resolve(TASKS.slice(0, 3));
+  }
 }
